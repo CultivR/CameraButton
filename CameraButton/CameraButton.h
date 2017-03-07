5e7eb95948708dd1c4c4b774b00d34d873379ccd
Jordan Kay <jordanekay@mac.com>
2017-03-07 10:52:27 -0800

    Initial Commit

---
+++ CameraButton/CameraButton.h
@@ -0,0 +1,19 @@
+//
+//  CameraButton.h
+//  CameraButton
+//
+//  Created by Jordan Kay on 3/7/17.
+//  Copyright © 2017 Squareknot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraButton.
+FOUNDATION_EXPORT double CameraButtonVersionNumber;
+
+//! Project version string for CameraButton.
+FOUNDATION_EXPORT const unsigned char CameraButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraButton/PublicHeader.h>
+
+
